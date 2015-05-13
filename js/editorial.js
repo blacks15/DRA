3ad1d28bbfedcb851dc2fec18063d49ee8e58841
blacks15 <felipe_borre@hotmail.com>
2015-05-13 17:20:58 -0600

    se agrego editorial.php
    se agrego el insert para editoriales aun no funciona correctamente

--- js/editorial.js
+++ js/editorial.js
@@ -1,26 +1,25 @@
 $(document).ready(function(){
 		
-	$("#alta").click(function(){
-		var first_name = $("#nombre").val();
-		var last_name = $("#apellido").val();
+	$("#altaeditorial").click(function(){
+		var name = $("#nombre").val();
 
 			$.ajax({
 				cache: false,
 				type: "POST",
 				datatype: "json",
-				url: "../php/autor.php",
-				data: {opc:"guardar_autor", first_name: first_name, last_name: last_name },
+				url: "../php/editorial.php",
+				data: {opc:"guardar_editorial", name: name },
 				success: function(response)
 				{
 					if(response.respuesta == true)
 					{
-						$(".mensajealta").html("Autor Registrado");
+						$(".mensajealta").html("Editorial Registrado");
 						$("#nombre").val("");
 						$("#apellido").val("");
 					}
 					else
 					{
-						$(".mensajealta").html("Autor No Registrado");
+						$(".mensajealta").html("Editorial No Registrado");
 					}
 				},	
 					error: function(xhr,ajaxOptions,throwError)
--- php/editorial.php
+++ php/editorial.php
@@ -1,25 +1,25 @@
 <?php
-	$opc = $_POST["opc"];
+	$opc = $_POST['opc'];
 	switch ($opc) 
 	{
-		case 'guardar_autor':
-			guardar_autor();
+		case 'guardar_editorial':
+			guardar_editorial();
 		break;
 		
-		case 'baja_autor':
-			baja_autor();
+		case 'baja_editorial':
+			baja_editorial();
 		break;
 
-		case 'consultar_autor':
-			consultar_autor();
+		case 'consultar_editorial':
+			consultar_editorial();
 		break;
 
-		case 'modificar_autor':
-			modificar_autor();
+		case 'modificar_editorial':
+			modificar_editorial();
 		break;
 	}
 
-	function guardar_autor()
+	function guardar_editorial()
 	{
 		$conecta = mysql_connect("localhost","root","")or die(mysql_error());
 
@@ -33,21 +33,20 @@
 			echo "Error al Conectar Base de Datos";
 		}
 		else {
-		$first_name = $_POST['first_name'];
-		$last_name = $_POST['last_name'];
+		$name = $_POST['name'];
 		$estado = 'Activo';
 
-		$consulta = "insert into autores (firstname_autor,lastname_autor,estado) values('".$first_name."','".$last_name."','".$estado."')";
+		$consulta = "insert into editorial (nombre_editorial,estado) values('".$name."','".$estado."')";
 		//ejecutar consulta
 		$resultado = mysql_query($consulta) or die(mysql_error());
 		$respuesta = false;
-
+		echo $name;
 		if (mysql_affected_rows() > 0)
 		{
 			$respuesta = true;
 			echo '<div class="alert alert-success">
 						  <button type="button" class="close" data-dismiss="alert">X</button>
-						  <strong>Autor '.$first_name." ".$last_name.' </strong> Guardado con Exito
+						  <strong>Autor '.$name.' </strong> Guardado con Exito
 						</div>';
 			$salidaJSON = array('respuesta' => $respuesta );
 			print json_encode($salidaJSON);
